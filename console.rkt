b8bdca997041cda1e80874a9bff52f01b7d58a63
Trevor Bramwell <trevor@bramwell.net>
2016-01-10 21:26:11 -0800

    Mail REPL
    After running 'racket console.rkt <username> <password> [-s <server>]'
    the user is provided a shell prompt. From here they can use the
    commands:
    
     - ?: Print a list of commands and their descriptions
     - l: List messages in the current mailbox (Only INBOX right now)
     - m: List mailboxes on the server
     - q: Disconnect from IMAP and exit the program
    
    Signed-off-by: Trevor Bramwell <trevor@bramwell.net>

--- console.rkt
+++ console.rkt
@@ -19,29 +19,64 @@
   (username imap-username)
   (password imap-password))
 
-(displayln (string-append "-- DEBUG: Connecting to " (server)))
+(define (display-message-list)
+    (for ([header (headers imap-connection total-messages)] [i (in-range total-messages)])
+         (display (+ 1 i))
+         (display " - ")
+         (displayln (get-subject (second header)))))
+
+(define (display-help)
+  (displayln "l - List messages")
+  (displayln "# - Read message #")
+  (displayln "q - Quit"))
+
+(define (display-mailboxes)
+  (display "Mailboxes: ")
+  (displayln mailboxes))
+
+(define (display-message idx)
+  (define (message-out-of-bounds)
+    (displayln "Message index out of bounds"))
+  (cond
+    [(<= idx 0) (message-out-of-bounds)]
+    [(>= total-messages idx)
+       (displayln
+         (caar (get-imap-message imap-connection (list idx))))]
+    [else (message-out-of-bounds)]))
+
+(define (disconnect-and-exit)
+  (rmail-disconnect imap-connection)
+  (exit))
+
+(define (next-input port)
+  (display "> ")
+  (let ([s (string-downcase (read-line port))])
+    (cond
+      [(string=? s "m") (display-mailboxes)]
+      [(string=? s "l") (display-message-list)]
+      [(string=? s "?") (display-help)]
+      [(string=? s "q") (disconnect-and-exit)]
+      [(number? (string->number s)) (display-message (string->number s))]
+      [else (displayln "Unrecognized input")])
+    (next-input port)))
+
+;
+; Debug Output
+;
+(displayln (string-append "# Connecting to " (server) " ..."))
 
 (define-values [imap-connection total-messages recent-messages]
   (rmail-connect server username password))
 
+(displayln "# Connection established.")
+
 (define-values [mailboxes]
   (rmail-mailboxes imap-connection))
+
 ;
-; Output
+; Mail REPL
 ;
+(next-input (current-input-port))
 
-(display "Mailboxes: " )
-(displayln mailboxes)
-
-(displayln "Messages: ")
-(for ([header (headers imap-connection total-messages)])
-     (display " - ")
-     (display (first header))
-     (display " ")
-     (displayln (get-subject (second header))))
-
-(displayln "First message: ")
-(displayln (caar (get-imap-message imap-connection (list 1))))
-
-(displayln "-- DEBUG: Disconnected")
-(rmail-disconnect imap-connection)
+;(displayln "-- DEBUG: Disconnected")
+;(rmail-disconnect imap-connection)
