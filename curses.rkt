644b2fb1cc16c7cb2c040786f699245f2fc9ff76
Trevor Bramwell <trevor@bramwell.net>
2016-03-08 08:20:23 -0800

    Update curses interface to output messages
    Signed-off-by: Trevor Bramwell <trevor@bramwell.net>

--- curses.rkt
+++ curses.rkt
@@ -1,7 +1,31 @@
-#lang racket/base
+#lang racket
 
 (require "lib/curses.rkt")
 (require "mail.rkt")
+
+(define (display-message-list imap win)
+    (let ([imap-connection (rmail-connection imap)]
+          [total-messages (rmail-total-messages imap)])
+      (for ([header (headers imap-connection total-messages)] [i (in-range total-messages)])
+           (waddstr win (number->string (+ 1 i)))
+           (waddstr win " - ")
+           (waddstr win (get-subject (second header)))
+           (waddstr win "\n"))))
+
+; Command Line Arguments
+;
+(command-line
+  #:program "rmail"
+  #:once-each
+  [("-s" "--server") imap-server
+                    "IMAP Server"
+                    (server imap-server)]
+  #:args (imap-username imap-password)
+  (username imap-username)
+  (password imap-password))
+
+(define list-item 1 )
+(define list-max 10)
 
 ;
 ; To get character-at-a-time input without echoing (most
@@ -19,22 +43,19 @@
 ;; Bounded cursor rows
 (define (inc-row! row)
   (cond
-    [(>= row 10) 10]
+    [(>= row list-max) list-max]
     [else (+ row 1)]))
 
 (define (dec-row! row)
   (cond
     [(<= row 1) 1]
     [else (- row 1)]))
-;
-
 
 (define win (initscr))
-(define list-item 1)
 
 (void (cbreak))
 (void (noecho))
-(void (waddstr win "Hello\n"))
+(void (start_color))
 
 (define (next-input win list-item)
   (let ([c (integer->char (wgetch win))])
@@ -42,12 +63,14 @@
         [(char=? c #\q) (void (endwin)) (exit)]
         [(char=? c #\k) (set! list-item (inc-row! list-item))]
         [(char=? c #\j) (set! list-item (dec-row! list-item))]
-        [(char=? c #\p) (waddstr win (number->string list-item))]
-        [else (void (waddstr win (string c)))])
+        [(char=? c #\p) (waddstr win (number->string list-item))])
     (void (wrefresh win))
     (next-input win list-item)))
 
-(next-input win list-item)
+; Setup connection
+(define current-imap (make-rmail (server) (username) (password)))
+(set! list-max (rmail-total-messages current-imap))
 
-; (sleep 1)
-; (void (endwin))
+(display-message-list current-imap win)
+
+(next-input win list-item)
